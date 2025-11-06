56538a5f5c5dafd111123288ce8d3becc6956b95
Lucas Dias <luucasdias@icloud.com>
2025-11-06 08:46:40 -0300

    Primeiro commit

---
+++ src/main/java/com/lucas/granjapatos/GranjapatosApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.granjapatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjapatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjapatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/granjapatos/GranjapatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.granjapatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjapatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
